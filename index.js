38c64fab4e73a6d329213c72ba0160f4a6797f29
Juan <juanangelbasgall@hotmail.com>
2024-02-08 14:50:08 -0300

    alcanzada la clase 5 de graph ql

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import { gql, ApolloServer } from "apollo-server";
+
+/**
+ * Puedo usar los modulos de EMC6 porque tengo en el pakacge.json el 
+ * "type": "module", entonces no import con el "require"
+ */
+
+import { gql, UserInputError, ApolloServer } from "apollo-server";
+import { v1 as uuid } from 'uuid'
+import axios from 'axios'
 
 const persons = [
     {
@@ -40,6 +48,11 @@ const persons = [
 
 
 const typeDefinitions = gql`
+    enum YesNo{
+        YES
+        NO
+    }
+
     type Address {
         street: String!
         city: String!
@@ -56,15 +69,40 @@ const typeDefinitions = gql`
 
     type Query {
         personCount: Int!
-        allPersons: [Person]!
+        allPersons(phone: YesNo): [Person]!
         findPerson(name: String!): Person
+    }
+
+    type Mutation{
+        addPerson(
+            name: String!
+            phone: String
+            street: String!
+            city: String!
+        ): Person
+        editNumber(
+            name: String!
+            phone: String!
+        ): Person
     }
 `
 
 const resolvers = {
     Query: {
         personCount: ()=> persons.length,
-        allPersons: ()=> persons,
+        // allPersons: ()=> persons,
+        allPersons: async (root, args)=> {
+            const {data: personsFromRestApi} = await axios.get('http://localhost:3000/persons')
+            if (!args.phone) return personsFromRestApi
+
+            return persons.filter(person=>{
+                return args.phone === "YES" ? person.phone : !person.phone
+            })
+
+            // const byPhone = person => 
+            //     args.phone === "YES" ? person.phone : !person.phone
+            // return persons.filter(byPhone)
+        },
         // allPersons: ()=> {
         //     return persons.map(person => ({
         //         id: person.id,
@@ -77,6 +115,30 @@ const resolvers = {
         findPerson: (root, args)=>{
             const {name} = args
             return persons.find(person=> person.name === name)
+        },
+        
+    },
+    Mutation: {
+        addPerson: (root, args)=>{
+            if (persons.find(p=> p.name === args.name)){
+                throw new UserInputError("Name must be unique", {
+                    invalidArgs: args.name
+                })
+            }
+            // const { name, phone, street, city } = args
+            const person = {...args, id: uuid()}
+            persons.push(person) // update database with new person
+            return person
+        },
+        editNumber: (root, args) => {
+            const personIndex = persons.findIndex(p => p.name === args.name)
+            if (!personIndex === -1) return null
+
+            const person = persons[personIndex]
+
+            const updatedPerson = {...person, phone: args.phone}
+            persons[personIndex] = updatedPerson
+            return updatedPerson
         }
     },
     Person: {
@@ -105,3 +167,6 @@ server.listen().then(({url})=>{
 
 // https://www.youtube.com/watch?v=QG-qbmW-wes
 // https://www.youtube.com/watch?v=3vldzoNRz-8
+// 
+// 
+// https://www.youtube.com/watch?v=ndQ6nWeQ7A0&list=PLV8x_i1fqBw0Kn_fBIZTa3wS_VZAqddX7&index=47
